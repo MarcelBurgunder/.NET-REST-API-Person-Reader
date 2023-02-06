82dc003f167aa9351cf5c32a4febd8b8e1c3516a
Marcel Burgunder <45422162+MarcelBurgunder@users.noreply.github.com>
2023-02-06 19:39:10 +0100

    Update Person.cs

--- PersonApi/Models/Person.cs
+++ PersonApi/Models/Person.cs
@@ -1,8 +1,26 @@
 namespace PersonApi.Models;
 
+// Person class respresenting a single person, with their full name and phone number
 public class Person
 {
+    // Member Properties
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string PhoneNumber { get; set; }
+    
+    // Empty Constructor - sets all properties to an empty string
+    public Person()
+    {
+        this.FirstName = "";
+        this.LastName = "";
+        this.PhoneNumber = "";
+    }
+    
+    // Main constructor - sets properties upon creation 
+    public Person(string firstName, string lastName, string phoneNumber)
+    {
+        this.FirstName = firstName;
+        this.LastName = lastName;
+        this.PhoneNumber = phoneNumber;
+    }
 }
