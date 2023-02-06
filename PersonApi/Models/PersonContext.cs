66e60aa100720114dda13ca6b12dce3c124df145
Marcel Burgunder <45422162+MarcelBurgunder@users.noreply.github.com>
2023-02-06 19:39:51 +0100

    Update PersonContext.cs

--- PersonApi/Models/PersonContext.cs
+++ PersonApi/Models/PersonContext.cs
@@ -2,6 +2,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace PersonApi.Models;
 
+// Database context class for the PersonApi controller - main data model holding a list of Persons
 public class PersonContext : DbContext
 {
     public PersonContext(DbContextOptions<PersonContext> options)
