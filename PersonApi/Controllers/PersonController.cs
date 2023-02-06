ec84a64192012828e6358a9673bc1b94ede47ad3
Marcel Burgunder <45422162+MarcelBurgunder@users.noreply.github.com>
2023-02-06 20:22:59 +0100

    Update PersonController.cs

--- PersonApi/Controllers/PersonController.cs
+++ PersonApi/Controllers/PersonController.cs
@@ -2,52 +2,54 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using PersonApi.Models;
 
-namespace PersonApi.Controllers;
-
-// Cnotroller Class
-[Route("api/[controller]")]
-[ApiController]
-public class PersonController : ControllerBase
+namespace PersonApi.Controllers
 {
-    // Controller Contexts holding the data model
-    private readonly PersonContext _context;
-
-    // Controller Constructor - Sets the context
-    public PersonController(PersonContext context)
+    // Controller Class
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
     {
-        _context = context;
-    }
-
-    // API Endpoint (Post Method) that takes in first and last name and phone number, and saves this to the PersonContext holding Persons.
-    [HttpPost]
-    public async ActionResult PostPerson(string FirstName, string LastName and string PhoneNumber)
-    {
-        Person pers = new Person(FirstName, LastName, PhoneNumber);
-        _context.Person.Add(pers);
-        await _context.SaveChangesAsync();
-        return CreatedAtAction("None", pers); // currently no action exists to retrieve the saved objects, hence first parameter is "None"
-    }
+        // Controller Contexts holding the data model
+        private readonly PersonContext _context;
     
-    // API Endpoint (Get Method) that takes in a list of persons via a file and logs each line, and returns processed file as response.
-    [HttpGet]
-    public async Task<ActionResult<string>> ReadFile(this IFormFile file)
-    {
-        if (file == null || file.Length == 0)
+        // Controller Constructor - Sets the context
+        public PersonController(PersonContext context)
         {
-            return BadRequest();
+            _context = context;
         }
-        
-        var result = new StringBuilder();
-        using (var reader = new StreamReader(file.OpenReadStream()))
+
+        // API Endpoint (Post Method) that takes in first and last name and phone number, and saves this to the PersonContext holding Persons.
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<ActionResult<Person>> PostPerson(string FirstName, string LastName, string PhoneNumber)
         {
-            while (reader.Peek() >= 0)
+            Person pers = new Person(FirstName, LastName, PhoneNumber);
+            _context.Person.Add(pers);
+            await _context.SaveChangesAsync();
+            return CreatedAtAction("None", pers); // currently no action exists to retrieve the saved objects, hence first parameter is "None"
+        }
+
+        // API Endpoint (Get Method) that takes in a list of persons via a file and logs each line, and returns processed file as response.
+        [HttpGet]
+        public async Task<ActionResult<string>> ReadFile(this IFormFile file)
+        {
+            if (file == null || file.Length == 0)
             {
-                string line = await reader.ReadLineAsync()
-                result.AppendLine(line);
-                Console.WriteLine(line)
+                return BadRequest();
             }
+
+            var result = new StringBuilder();
+            using (var reader = new StreamReader(file.OpenReadStream()))
+            {
+                while (reader.Peek() >= 0)
+                {
+                    string line = await reader.ReadLineAsync();
+                    result.AppendLine(line);
+                    Console.WriteLine(line);
+                }
+            }
+
+            return result.ToString();
         }
-        
-        return result.ToString();
     }
-}
+};
