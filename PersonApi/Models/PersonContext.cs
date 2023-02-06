f6e3ac7d11b01b2f873b02252e0679d016de9893
Marcel Burgunder <45422162+MarcelBurgunder@users.noreply.github.com>
2023-02-06 20:09:54 +0100

    Update PersonContext.cs
    Database context for Person API

--- PersonApi/Models/PersonContext.cs
+++ PersonApi/Models/PersonContext.cs
@@ -5,10 +5,12 @@ namespace PersonApi.Models;
 // Database context class for the PersonApi controller - main data model holding a list of Persons
 public class PersonContext : DbContext
 {
+    // Empty constructor - no other constructors necessary for contexts construction
     public PersonContext(DbContextOptions<PersonContext> options)
         : base(options)
     {
     }
 
+    // List of saved persons - in a DbSet object
     public DbSet<Person> Persons { get; set; } = null!;
 }
