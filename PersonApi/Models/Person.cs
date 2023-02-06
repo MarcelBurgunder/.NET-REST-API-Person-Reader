a79295ba607546373d175c358bcddf785af9896f
Marcel Burgunder <45422162+MarcelBurgunder@users.noreply.github.com>
2023-02-06 19:21:37 +0100

    Create Person.cs

---
+++ PersonApi/Models/Person.cs
@@ -0,0 +1,8 @@
+namespace PersonApi.Models;
+
+public class Person
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+}
