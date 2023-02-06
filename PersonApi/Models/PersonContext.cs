0fceb26cc92cb64ade7c40c14092c2c0c001a935
Marcel Burgunder <45422162+MarcelBurgunder@users.noreply.github.com>
2023-02-06 20:24:16 +0100

    Update PersonContext.cs

--- PersonApi/Models/PersonContext.cs
+++ PersonApi/Models/PersonContext.cs
@@ -1,16 +1,17 @@
 using Microsoft.EntityFrameworkCore;
 
-namespace PersonApi.Models;
-
-// Database context class for the PersonApi controller - main data model holding a list of Persons
-public class PersonContext : DbContext
-{
-    // Empty constructor - no other constructors necessary for contexts construction
-    public PersonContext(DbContextOptions<PersonContext> options)
-        : base(options)
+namespace PersonApi.Models
     {
-    }
+    // Database context class for the PersonApi controller - main data model holding a list of Persons
+    public class PersonContext : DbContext
+    {
+        // Empty constructor - no other constructors necessary for contexts construction
+        public PersonContext(DbContextOptions<PersonContext> options)
+            : base(options)
+        {
+        }
 
-    // List of saved persons - in a DbSet object
-    public DbSet<Person> Persons { get; set; } = null!;
-}
+        // List of saved persons - in a DbSet object
+        public DbSet<Person> Persons { get; set; } = null!;
+    }
+};
